643f6ccd9cb46de940799306b2aef09920e480d6
richardnseven <richardnseven@gmail.com>
2020-02-11 18:05:34 +0000

    Processer.py

--- venv/Model/Processer.py
+++ venv/Model/Processer.py
@@ -15,7 +15,25 @@ def creatProject():
     project = P.Project("project", "primary", "second", "type")
     projectList.append(project)
 
+def countLabel():
+    "在这里对label计数"
+    primaryLabel = {"A": dict(), "B": 0}#对一级标签的计数
+    secondA = dict()
+
+    for p in projectList:
+        label = p.getLabel()
+        primary = label["primary"]
+        second = label["second"]
+        type = label["type"]
+
+
+        if primary == "A":
+            primaryLabel["A"] += 1
+
+
+
 creatStudent()
+countLabel()
 print(studentList[0].getID())
 creatProject()
 print(projectList[0])
